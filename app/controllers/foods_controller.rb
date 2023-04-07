bb2c26410d2b7c1ab058fc587b58d52b1959d835
Dawson Timmons <DawsonTimmons@gmail.com>
2023-04-07 10:25:20 -0600

    feat: redirect the user to /foods and display the search results

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   def index
-    
+    @foods = FoodFacade.get_foods(params[:search])
+    @results = FoodFacade.get_foods_results_count(params[:search])
   end
 end
